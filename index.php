c9f4844f0b1060b8e219dfaa49447f8cd2716e99
Jesse <jesseluza7@gmail.com>
2024-12-01 16:02:11 -0300

    segundo teste

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Testando Vercel</title>
 </head>
 <body>
-    <h1>
-        Hello World
-    </h1>
+    <p>TESTANDO VERCEL</p>
 </body>
 </html>
